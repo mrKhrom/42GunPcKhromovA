49d12d405eb2d66956873e17cf3341a6096049d4
Aleksey Khromov <khromofficial@icloud.com>
2026-05-20 00:33:06 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!!");
+
+var a = Int32. Parse(Console.ReadLine());
+var b = Int32. Parse(Console.ReadLine());
+Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
+Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+Console.WriteLine("Result of {0} % {1} = {2}", a, b, a % b);
