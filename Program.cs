4ad5965e6744ad11877941409778ff350c197765
Aleksey Khromov <khromofficial@icloud.com>
2026-05-28 23:39:20 +0300

    Выполнение задания циклы

--- Program.cs
+++ Program.cs
@@ -1,48 +1,57 @@
-﻿// Task 1
-int[] febonachi = new int[8] {0, 1, 1, 2, 3, 5, 8, 13};
+﻿using System;
 
-// Task 2
-string[] monthes = new string[12] {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
-// Task 3
-// 2 3 4
-// 4 9 16
-// 8 27 64
-int[,] numbers = new int[3, 3] { {2, 3, 4}, {4, 9, 16}, {8, 27, 64} };
-
-// Task 4
-
-// Jagged array (ломанный массив)
-double[][] jaggedArray = new double[3][];
-
-// Первый массив - числа от 1 до 5
-jaggedArray[0] = new double[] { 1, 2, 3, 4, 5 };
-
-// Второй массив - константы e и pi
-jaggedArray[1] = new double[] { Math.E, Math.PI };
-
-// Третий массив - логарифм по основанию 10 чисел 1, 10, 100 и 1000
-jaggedArray[2] = new double[]
+class Program
 {
-	Math.Log10(1),
-	Math.Log10(10),
-	Math.Log10(100),
-	Math.Log10(1000)
-};
+	static void Main(string[] args)
+	{
+		// Task 1
+		Console.WriteLine("Task 1");
+		int n1 = 0, n2 = 1, n3;
+		Console.WriteLine(n1);
+		Console.WriteLine(n2);
+		for (int i = 2; i < 10; i++)
+		{
+			n3 = n1 + n2;
+			Console.WriteLine(n3);
+			n1 = n2;
+			n2 = n3;
+		}
 
-// Task 5
-int[] array = { 1, 2, 3, 4, 5 }; 
-int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
 
-Array.Copy(array, 0, array2, 0, 3);
+		// Task 2
+		Console.WriteLine("Task 2");
+		for (int i = 1; i <= 20; i++)
+		{
+			if(i%2 == 0)
+				Console.WriteLine(i);
+		}
 
-Console.WriteLine("Array2 after copy:");
-foreach (int i in array2)
-    Console.WriteLine(i);
-
-//Task 6
-Array.Resize(ref array, array.Length * 2);
-
-Console.WriteLine("Array1 after resizing:");
-foreach (int i in array)
-    Console.WriteLine(i);
+		// Task 3
+		Console.WriteLine("Task 3");
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; j <= 5; j++)
+			{
+				Console.Write((i * j).ToString().PadLeft(3));
+			}
+			Console.WriteLine();
+		}
+		// Task 4
+		Console.WriteLine("Task 4");
+		string password = "qwerty";
+		do
+		{
+			Console.Write("Enter password: ");
+			string input = Console.ReadLine();
+			if (input == password)
+			{
+				Console.WriteLine("Access granted.");
+				break;
+			}
+			else
+			{
+				Console.WriteLine("Access denied. Try again.");
+			}
+		} while (true);
+	}
+}
